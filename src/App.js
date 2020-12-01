8c2ce3dad3ea010d4b252b92a4e5abcba61c8da2
Emmanuel Worgu <emmanuelworgu@gmail.com>
2020-11-30 17:20:02 -0800

    api connected successfully

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import Nav from './Nav';
 import Body from './Body';
 import Footer from './Footer';
+import Loading from './Loading';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
@@ -9,17 +10,43 @@ function App() {
  const ApiKey = '56f6937a1d052f6ce1e0056c291d24b3';
 
   const[locality, setLocality] = useState('');
-  const[apiData, setApiData] = useState([]);
+  const[apiData, setApiData] = useState({
+    loading: false,
+    data: []
+  });
 
   const localHandler = (e) => {
       setLocality(e.target.value);
   };
 
-  const handleForm = (e) => {
+  const handleForm = async (e) => {
       e.preventDefault();
-      // console.log(locality);
+      setApiData({
+        loading: true,
+        data: []
+      });
+      //console.log(apiData);
+      try {
+        const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.openweathermap.org/data/2.5/weather?p=${locality}&appid=${ApiKey}`);
+        setApiData({
+          loading: true
+        });
+        const data = await response.json();
+        setApiData({
+          loading: false,
+          data: data
+        });
+        console.log(apiData);
+      } catch (error) {
+          setApiData({
+            loading: false,
+            data: 'An error occured'
+          });
+          console.log(apiData.data);
+      }
       console.log('Working');
   };
+
 
   // Get User Location And display weather based on their location
   const fetchData = () => {
@@ -62,32 +89,26 @@ function App() {
 
 
   // Handle What will be display on the screen when the user searches through the input and also press the submit button
-  const logic = () => {
-    if (locality === '') {
-      return (
-        <p>This is the weather in your location</p>
-      );
-    } else if (handleForm === false) {
-      return (
-        <p>This is the weather in your location</p>
-      );
-    } else {
-      return (
-        <p>This is the weather in {locality} </p>
-      );
-    };
-  };
+  // const logic = () => {
+  //   <p>{apiData.data}</p>
+  // };
   // This will save and store the user searches on local storage
   localStorage.setItem("recentSearched", locality);
   const recent = localStorage.getItem("recentSearched");
-  
-  return (
-    <div>
-      <Nav value={locality} localHandler={localHandler} handleForm={handleForm}/>
-      <Body message={logic()}/>
-      <Footer/>
-    </div>
-  );
+
+  if (apiData.loading) {
+    return (
+      <Loading/>
+    );
+  } else {
+      return (
+        <div>
+          <Nav value={locality} localHandler={localHandler} handleForm={handleForm}/>
+          <Body message={apiData.data}/>
+          <Footer/>
+        </div>
+      );
+  }
 }
 
 export default App;
---
+++ src/Loading.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './loading.css';
+
+function Loading() {
+    return (
+        <div>
+            <h4>Please Wait why we get the information...</h4>
+        </div>
+    );
+};
+
+export default Loading;
